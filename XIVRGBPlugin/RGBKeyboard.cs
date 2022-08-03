fa10b5f892a5723f6375d8ae931b66f8203310e9
Enrique Sandoval <ee.sandoval.jr@gmail.com>
2022-08-02 22:27:52 -0700

    RGB Keyboard of IRGBDevice

---
+++ XIVRGBPlugin/RGBKeyboard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XIVRGBPlugin
+{
+    public class RGBKeyboard : IRGBDevice
+    {
+        public List<RGBLed> RGBLeds { get => RGBLeds; set => RGBLeds = value; }
+
+        public void SetSingleColor(int r, int g, int b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetSingleColorPulseEffect(int r, int g, int b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetSingleColorWaveEffect(int r, int g, int b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
